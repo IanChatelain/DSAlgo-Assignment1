1413c5b0bd500523cbfd0fa55a4139e4fcd38f79
Ian Chatelain(RRC) <118557647+IanChatelain@users.noreply.github.com>
2024-01-23 11:19:59 -0600

    Assignment 1 - Milestone 2 (Started)

---
+++ DS Assignment 1/Employee.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    /// <summary>
+    /// Represents an employee with an ID, first name, and last name.
+    /// </summary>
+    public class Employee : IComparable<Employee>
+    {
+        public int EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Employee class with the specified ID.
+        /// </summary>
+        /// <param name="employeeId">The unique identifier for the new employee.</param>
+        public Employee(int employeeId)
+        {
+            this.EmployeeID = employeeId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Employee class with the specified ID, first name, and last name.
+        /// </summary>
+        /// <param name="employeeId">The unique identifier for the new employee.</param>
+        /// <param name="firstName">The first name of the new employee.</param>
+        /// <param name="lastName">The last name of the new employee.</param>
+        public Employee(int employeeId, string firstName, string lastName)
+        {
+            this.EmployeeID = employeeId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        /// <summary>
+        /// Compares the current Employee instance with another Employee object for sort order.
+        /// </summary>
+        /// <param name="other">The Employee to compare with this instance.</param>
+        /// <returns>
+        /// An integer that indicates the order of the employees being compared.
+        /// </returns>
+        public int CompareTo(Employee other)
+        {
+            return this.EmployeeID.CompareTo(other.EmployeeID);
+        }
+
+        /// <summary>
+        /// Returns a string representation of the current Employee object.
+        /// </summary>
+        /// <returns>
+        /// A string containing the employee's ID, first name, and last name. 
+        /// </returns>
+        public override string ToString()
+        {
+            string firstName = FirstName ?? "null";
+            string lastName = LastName ?? "null";
+
+            return string.Format("{0} {1} {2}", EmployeeID, firstName, lastName);
+        }
+    }
+}
---
+++ DS Assignment 1/LinkedList.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    /// <summary>
+    /// Represents a generic doubly linked list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the linked list.</typeparam>
+    public class LinkedList<T> where T : IComparable<T>
+    {
+        // Milestone #1
+
+        public Node<T>? Head { get; set; }
+        public Node<T>? Tail { get; set; }
+        public int Size { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the LinkedList class.
+        /// </summary>
+        public LinkedList()
+        {
+            this.Clear();
+        }
+
+        /// <summary>
+        /// Removes all nodes from the linked list.
+        /// </summary>
+        public void Clear()
+        {
+            this.Size = 0;
+            this.Head = null;
+            this.Tail = null;
+        }
+
+        /// <summary>
+        /// Determines whether the linked list is empty.
+        /// </summary>
+        /// <returns>True if the linked list is empty, otherwise, false.</returns>
+        public bool IsEmpty()
+        {
+            return Size == 0;
+        }
+
+        /// <summary>
+        /// Gets the element in the first node of the linked list.
+        /// </summary>
+        /// <returns>The element in the first node of the linked list.</returns>
+        /// <exception cref="ApplicationException">Thrown if the linked list is empty.</exception>
+        public T GetFirst()
+        {
+            this.ThrowIsEmpty();
+            return this.Head.Element;
+        }
+
+        /// <summary>
+        /// Gets the element in the last node of the linked list.
+        /// </summary>
+        /// <returns>The element in the last node of the linked list.</returns>
+        /// <exception cref="ApplicationException">Thrown if the linked list is empty.</exception>
+        public T GetLast()
+        {
+            this.ThrowIsEmpty();
+            return this.Tail.Element;
+        }
+
+        /// <summary>
+        /// Replaces the element in the first node and returns the old element.
+        /// </summary>
+        /// <param name="element">The new element to set in the first node.</param>
+        /// <returns>The old element that was replaced.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the provided element is null.</exception>
+        /// <exception cref="ApplicationException">Thrown if the linked list is empty.</exception>
+        public T SetFirst(T element)
+        {
+            this.ThrowNotNull(element);
+            this.ThrowIsEmpty();
+
+            T oldElement = this.Head.Element;
+
+            this.Head.Element = element;
+
+            return oldElement;
+        }
+
+        /// <summary>
+        /// Replaces the element in the last node and returns the old element.
+        /// </summary>
+        /// <param name="element">The new element to set in the last node.</param>
+        /// <returns>The old element that was replaced.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if the provided element is null.</exception>
+        /// <exception cref="ApplicationException">Thrown if the linked list is empty.</exception>
+        public T SetLast(T element)
+        {
+            this.ThrowNotNull(element);
+            this.ThrowIsEmpty();
+
+            T oldElement = this.Tail.Element;
+
+            this.Tail.Element = element;
+
+            return oldElement;
+        }
+
+        /// <summary>
+        /// Adds a new node containing the specified element to the start of the linked list.
+        /// </summary>
+        /// <param name="element">The element to add to the linked list.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the provided element is null.</exception>
+        public void AddFirst(T element)
+        {
+            this.ThrowNotNull(element);
+
+            Node<T> newNode = new Node<T>(element);
+
+            if (this.IsEmpty())
+            {
+                this.Head = newNode;
+                this.Tail = newNode;
+            }
+            else
+            {
+                newNode.Next = this.Head;
+                this.Head.Previous = newNode;
+                this.Head = newNode;
+            }
+
+            this.Size++;
+        }
+
+        /// <summary>
+        /// Adds a new node containing the specified element to the end of the linked list.
+        /// </summary>
+        /// <param name="element">The element to add to the linked list.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the provided element is null.</exception>
+        public void AddLast(T element)
+        {
+            this.ThrowNotNull(element);
+
+            Node<T> newNode = new Node<T>(element);
+
+            if (this.IsEmpty())
+            {
+                this.Head = newNode;
+                this.Tail = newNode;
+            }
+            else
+            {
+                this.Tail.Next = newNode;
+                newNode.Previous = this.Tail;
+                this.Tail = newNode;
+            }
+
+            this.Size++;
+        }
+
+        /// <summary>
+        /// Checks if the provided element is null and throws an ArgumentNullException if it is.
+        /// </summary>
+        /// <param name="element">The element to check for null.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the provided element is null.</exception>
+        private void ThrowNotNull(T element)
+        {
+            if(element == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        /// <summary>
+        /// Checks if the linked list is empty and throws an ApplicationException if it is.
+        /// </summary>
+        /// <exception cref="ApplicationException">Thrown when the linked list is empty.</exception>
+        private void ThrowIsEmpty()
+        {
+            if (this.IsEmpty())
+            {
+                throw new ApplicationException("List is empty");
+            }
+        }
+
+        // Milestone #2
+
+        public T RemoveFirst()
+        {
+            return
+        }
+
+        public T RemoveLast()
+        {
+            return
+        }
+
+        public T Get(int position)
+        {
+            return
+        }
+
+        public T Remove(int position)
+        {
+            return
+        }
+
+        public void AddAfter(T element, int position)
+        {
+
+        }
+
+        public void AddBefore(T element, int position)
+        {
+
+        }
+    }
+}
