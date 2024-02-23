216e1c0e2f9e756c8d09f2dd119b340a81fb5e5b
Ian Chatelain(RRC) <118557647+IanChatelain@users.noreply.github.com>
2024-02-23 12:25:27 -0600

    Assignment 2 - Milestone 1: Setup & initial coding complete.

---
+++ DS Assignment 2/Point.cs
@@ -0,0 +1,20 @@
+﻿namespace TestLibrary
+{
+    public class Point
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Point(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
---
+++ DS Assignment 2/Stack.cs
@@ -0,0 +1,48 @@
+﻿namespace TestLibrary
+{
+    public class Stack<T>
+    {
+        public Node<T>? Head { get; set; }
+        public int Size { get; set; }
+
+        public Stack()
+        {
+            this.Clear();
+        }
+
+        public void Push(T element)
+        {
+            Node<T> newNode = new Node<T>(element);
+
+            this.Head = newNode;
+            this.Head.Next = null;
+        }
+
+        public T? Top()
+        {
+            return this.Head.Element;
+        }
+
+        public T? Pop()
+        {
+            T oldElement = this.Head.Element;
+
+            this.Head = this.Head.Next;
+
+            return oldElement;
+        }
+
+        public void Clear()
+        {
+            this.Head = null;
+            this.Head.Next = null;
+            this.Size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Size == 0 ? true : false;
+        }
+    }
+}
+
